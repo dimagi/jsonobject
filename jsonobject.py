2475e571287a614d808c46c72ecb4fbf8aeed6ff
Daniel Roberts <droberts@dimagi.com>
2013-07-17 10:31:31 -0400

    add support for renaming a json property (if the dict key and property name need to be different)

--- jsonobject.py
+++ jsonobject.py
@@ -3,11 +3,28 @@ import inspect
 
 class JsonProperty(object):
 
-    def __init__(self, default=None):
+    def __init__(self, default=None, name=None):
+        self.name = name
         if not callable(default):
             self.default = lambda: default
         else:
             self.default = default
+
+    def init_property(self, default_name):
+        self.name = self.name or default_name
+
+    def wrap(self, obj):
+        raise NotImplementedError()
+
+    def unwrap(self, obj):
+        raise NotImplementedError()
+
+    def __get__(self, instance, owner):
+        assert self.name in instance
+        return instance[self.name]
+
+    def __set__(self, instance, value):
+        instance[self.name] = value
 
 
 class AssertTypeProperty(JsonProperty):
@@ -16,9 +33,14 @@ class AssertTypeProperty(JsonProperty):
     def assert_type(self, obj):
         assert isinstance(obj, self._type), \
             '{} not of type {}'.format(obj, self._type)
+
+    def wrap(self, obj):
+        self.assert_type(obj)
         return obj
 
-    wrap = unwrap = assert_type
+    def unwrap(self, obj):
+        self.assert_type(obj)
+        return obj, obj
 
 
 class StringProperty(AssertTypeProperty):
@@ -30,11 +52,11 @@ class IntegerProperty(AssertTypeProperty):
 
 
 class ObjectProperty(JsonProperty):
-    def __init__(self, obj_type, default=Ellipsis):
+    def __init__(self, obj_type, default=Ellipsis, **kwargs):
         self._obj_type = obj_type
         if default is Ellipsis:
             default = self.obj_type
-        super(ObjectProperty, self).__init__(default=default)
+        super(ObjectProperty, self).__init__(default=default, **kwargs)
 
 
     @property
@@ -50,15 +72,15 @@ class ObjectProperty(JsonProperty):
     def unwrap(self, obj):
         assert isinstance(obj, self.obj_type), \
             '{} is not an instance of {}'.format(obj, self.obj_type)
-        return obj._obj
+        return obj, obj._obj
 
 
 class ListProperty(ObjectProperty):
 
-    def __init__(self, obj_type, default=Ellipsis):
+    def __init__(self, obj_type, default=Ellipsis, **kwargs):
         if default is Ellipsis:
             default = list
-        super(ListProperty, self).__init__(obj_type, default=default)
+        super(ListProperty, self).__init__(obj_type, default=default, **kwargs)
 
     def wrap(self, obj):
         return JsonArray(obj, wrapper=type_to_property(self.obj_type))
@@ -68,11 +90,11 @@ class ListProperty(ObjectProperty):
             '{} is not an instance of list'.format(obj)
 
         if isinstance(obj, JsonArray):
-            return obj._obj
+            return obj, obj._obj
         else:
             wrapped = self.wrap([])
             wrapped.extend(obj)
-            return wrapped, self.unwrap(wrapped)
+            return self.unwrap(wrapped)
 
 
 class JsonArray(list):
@@ -87,28 +109,46 @@ class JsonArray(list):
         return self._obj
 
     def append(self, wrapped):
-        self._obj.append(self._wrapper.unwrap(wrapped))
+        wrapped, unwrapped = self._wrapper.unwrap(wrapped)
+        self._obj.append(unwrapped)
         super(JsonArray, self).append(wrapped)
 
     def extend(self, wrapped_list):
-        self._obj.extend(map(self._wrapper.unwrap, wrapped_list))
+        if wrapped_list:
+            wrapped_list, unwrapped_list = zip(
+                *map(self._wrapper.unwrap, wrapped_list)
+            )
+        else:
+            unwrapped_list = []
+        self._obj.extend(unwrapped_list)
         super(JsonArray, self).extend(wrapped_list)
 
 
 class JsonObjectMeta(type):
     def __new__(mcs, name, bases, dct):
-        wrappers = {}
+        properties = {}
+        properties_by_name = {}
         for key, value in dct.items():
             if isinstance(value, JsonProperty):
-                wrappers[key] = value
-                del dct[key]
-        for base in bases:
-            if getattr(base, '_wrappers', None):
-                for key, value in base._wrappers.items():
-                    if key not in wrappers:
-                        wrappers[key] = value
+                properties[key] = value
+
         cls = type.__new__(mcs, name, bases, dct)
-        cls._wrappers = wrappers
+
+        for key, property_ in properties.items():
+            property_.init_property(default_name=key)
+            assert property_.name is not None, property_
+            assert property_.name not in properties_by_name, \
+                'You can only have one property named {}'.format(property_.name)
+            properties_by_name[property_.name] = property_
+
+        for base in bases:
+            if getattr(base, '_properties_by_attr', None):
+                for key, value in base._properties_by_attr.items():
+                    if key not in properties:
+                        properties[key] = value
+
+        cls._properties_by_attr = properties
+        cls._properties_by_key = properties_by_name
         return cls
 
 
@@ -116,63 +156,50 @@ class JsonObject(dict):
 
     __metaclass__ = JsonObjectMeta
 
-    _wrappers = None
+    _properties_by_attr = None
+    _properties_by_key = None
 
-    def __init__(self, **kwargs):
+    def __init__(self, _obj=None, **kwargs):
         super(JsonObject, self).__init__()
-        self._obj = kwargs
+
+        self._obj = _obj or {}
+
         for key, value in self._obj.items():
             self[key] = self.__wrap(key, value)
-        for key, value in self._wrappers.items():
+
+        for attr, value in kwargs.items():
+            assert attr in self._properties_by_attr
+            setattr(self, attr, value)
+
+        for key, value in self._properties_by_key.items():
             if key not in self._obj:
                 d = value.default()
                 self[key] = d
 
     @classmethod
     def wrap(cls, obj):
-        return cls(**obj)
+        self = cls(obj)
+        return self
 
     def to_json(self):
         return self._obj
 
-    def __get_wrapper(self, key):
-        try:
-            return self._wrappers[key]
-        except (TypeError, KeyError):
-            return None
-
     def __wrap(self, key, value):
-        wrapper = self.__get_wrapper(key)
-        if wrapper and value is not None:
-            return wrapper.wrap(value)
-        else:
-            return value
+        if value is None:
+            return None
+        return self._properties_by_key[key].wrap(value)
 
     def __unwrap(self, key, value):
-        wrapper = self.__get_wrapper(key)
-        if wrapper and value is not None:
-            return wrapper.unwrap(value)
-        else:
-            return value
+        if value is None:
+            return None, None
 
-    def __getattr__(self, item):
-        try:
-            return self[item]
-        except KeyError:
-            raise AttributeError(item)
-
-    def __setattr__(self, key, value):
-        if key in self._wrappers:
-            self[key] = value
-        else:
-            super(JsonObject, self).__setattr__(key, value)
+        return self._properties_by_key[key].unwrap(value)
 
     def __setitem__(self, key, value):
-        unwrapped = self.__unwrap(key, value)
-        if isinstance(unwrapped, tuple):
-            value, unwrapped = unwrapped
-        super(JsonObject, self).__setitem__(key, value)
+        wrapped, unwrapped = self.__unwrap(key, value)
+        super(JsonObject, self).__setitem__(key, wrapped)
         self._obj[key] = unwrapped
+
 
 TYPE_TO_PROPERTY = {
     int: IntegerProperty,
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
+from copy import deepcopy
 import unittest
-from jsonobject import JsonObject, StringProperty, ListProperty, JsonArray, ObjectProperty
+from jsonobject import JsonObject, StringProperty, ListProperty, JsonArray, ObjectProperty, IntegerProperty
 
 
 class Features(JsonObject):
@@ -18,6 +19,15 @@ class Person(JsonObject):
     def full_name(self):
         return u'{self.first_name} {self.last_name}'.format(self=self)
 
+
+class JunkCD(JsonObject):
+    c_property = IntegerProperty(name='c')
+    d_property = StringProperty(name='d')
+
+
+class JunkAB(JsonObject):
+    a_property = ListProperty(int, name='a')
+    b_property = ObjectProperty(JunkCD, name='b')
 
 class JsonObjectTestCase(unittest.TestCase):
     def test_wrap(self):
@@ -87,6 +97,60 @@ class JsonObjectTestCase(unittest.TestCase):
             'features': {'hair': None, 'eyes': None},
             'favorite_numbers': [],
         })
+
+    def test_name(self):
+        class Wack(JsonObject):
+            underscore_obj = StringProperty(name='_obj')
+        w = Wack()
+        self.assertEqual(w.to_json(), {'_obj': None})
+        w.underscore_obj = 'new_value'
+        self.assertEqual(w.underscore_obj, 'new_value')
+        self.assertEqual(w.to_json(), {'_obj': 'new_value'})
+
+    def test_mapping(self):
+
+        json_end = {
+            'a': [1, 2, 3],
+            'b': {
+                'c': 1,
+                'd': 'string',
+            }
+        }
+
+        p = JunkAB(deepcopy(json_end))
+        self.assertEqual(p.to_json(), json_end)
+        p.a_property.append(4)
+        self.assertEqual(p.to_json(), {'a': [1, 2, 3, 4], 'b': json_end['b']})
+        p.a_property = []
+        self.assertEqual(p.to_json(), {'a': [], 'b': json_end['b']})
+        p.a_property = None
+        self.assertEqual(p.to_json(), {'a': None, 'b': json_end['b']})
+        p['a'] = [1, 2, 3]
+        self.assertEqual(p.to_json(), json_end)
+        self.assertEqual(p.keys(), p.to_json().keys())
+
+    def test_competing_names(self):
+        with self.assertRaises(AssertionError):
+            class Bad(JsonObject):
+                a = IntegerProperty(name='ay')
+                eh = StringProperty(name='ay')
+
+    def test_init(self):
+        self.assertEqual(JunkCD(c_property=1, d_property='yyy').to_json(),
+                         JunkCD({'c': 1, 'd': 'yyy'}).to_json())
+        with self.assertRaises(AssertionError):
+            JunkCD(non_existent_property=2)
+
+        ab = JunkAB(a_property=[1, 2, 3],
+                    b_property=JunkCD({'c': 1, 'd': 'string'}))
+        self.assertEqual(ab.to_json(), {
+            'a': [1, 2, 3],
+            'b': {
+                'c': 1,
+                'd': 'string',
+            }
+        })
+
 
 
 if __name__ == '__main__':
