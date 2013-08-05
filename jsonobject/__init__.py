bc74c8ee15927457400b75edd2491c8bbfe52d27
Daniel Roberts <droberts@dimagi.com>
2013-08-05 00:37:42 -0400

    convert recognized types to properties
    class Foo(JsonObject):
        string = 'my value'
    
    now sets string to be StringProperty(default='my value')

--- jsonobject/__init__.py
+++ jsonobject/__init__.py
@@ -1,8 +1,11 @@
 from base import JsonObject, JsonArray
-from properties import *
+from .properties import *
+import properties
 __all__ = [
     'IntegerProperty', 'FloatProperty', 'StringProperty', 'BooleanProperty',
     'DateProperty', 'DateTimeProperty',
     'ObjectProperty', 'ListProperty',
     'JsonObject', 'JsonArray',
 ]
+
+JsonObjectMeta.properties_module = properties
