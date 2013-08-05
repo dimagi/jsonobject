3fcdf1ea17266dac9085d708876120a9931529c8
Daniel Roberts <droberts@dimagi.com>
2013-08-05 01:34:19 -0400

    add MANIFEST.in and bump version

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup
 
 setup(
     name='jsonobject',
-    version='0.0.1',
+    version='0.0.2',
     author='Danny Roberts',
     author_email='droberts@dimagi.com',
     description='A library for dealing with JSON as python objects',
     long_description=open('README.md').read(),
     url='https://github.com/dannyroberts/jsonobject',
     packages=['jsonobject'],
-    install_requires=[
-    ],
+    install_requires=[],
     tests_require=['unittest2'],
     test_suite='tests',
 )
