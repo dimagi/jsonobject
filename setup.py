dad131849443eaa4b7955605838cbe90af21f244
Daniel Roberts <droberts@dimagi.com>
2013-08-04 20:46:01 -0400

    add a skeleton setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='jsonobject',
+    version='0.0.1',
+    author='Danny Roberts',
+    packages=['jsonobject'],
+    install_requires=[
+    ]
+)
