831f4eab68823186795598f2e85df292c22e4fa6
Daniel Roberts <droberts@dimagi.com>
2013-12-13 17:41:54 -0500

    bump version to 0.2.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jsonobject',
-    version='0.2.9',
+    version='0.2.10',
     author='Danny Roberts',
     author_email='droberts@dimagi.com',
     description='A library for dealing with JSON as python objects',
