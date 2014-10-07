c27ad88406b290d4c94222f4ef0040ad1d4d955c
Aleksandar Yalnazov <yalnazov@gmail.com>
2014-10-07 23:21:40 +0300

    Initial port to Py3 done.

--- test/couchdbkit/couchdbkit.py
+++ test/couchdbkit/couchdbkit.py
@@ -1,6 +1,20 @@
 import functools
 from jsonobject import *
-
+import types
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
 SchemaProperty = ObjectProperty
 SchemaListProperty = ListProperty
 StringListProperty = functools.partial(ListProperty, unicode)
