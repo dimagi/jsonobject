887c28a7fb8d1b608eaeea22e03f784098dd8dce
Jonathan Emord <jonathan.emord@gmail.com>
2018-01-17 12:37:14 -0500

    Fix conditional

--- test_cython_files.sh
+++ test_cython_files.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ "$(python --version 2>&1 | cut -d'.' -f1,2)" = "Python 2.7" ]
+if [ "$(python --version 2>&1 | cut -d'.' -f1,2)" != "Python 2.7" ]
 then
   echo "Skipping cython build test on python 3"
   exit 0
