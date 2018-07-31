4cbeec5ad06d751d0073c33d93fe813dca09409e
Nick Pellegrino <npellegrino@dimagi.com>
2018-07-31 14:51:46 -0400

    bump version to 0.9.3

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ with io.open('README.md', 'rt', encoding="utf-8") as readme_file:
 
 setup(
     name='jsonobject',
-    version='0.9.2',
+    version='0.9.3',
     author='Danny Roberts',
     author_email='droberts@dimagi.com',
     description='A library for dealing with JSON as python objects',
