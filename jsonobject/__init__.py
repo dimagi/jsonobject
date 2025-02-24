769fa4a429d33a8d5844d40f189c831f265fc537
Daniel Miller <dmiller@dimagi.com>
2025-02-24 12:04:37 -0500

    Bump version to 2.3.0 and update change log

--- jsonobject/__init__.py
+++ jsonobject/__init__.py
@@ -2,7 +2,7 @@ from .containers import JsonArray
 from .properties import *
 from .api import JsonObject
 
-__version__ = '2.2.0'
+__version__ = '2.3.0'
 __all__ = [
     'IntegerProperty', 'FloatProperty', 'DecimalProperty',
     'StringProperty', 'BooleanProperty',
